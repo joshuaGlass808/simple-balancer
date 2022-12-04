cab5b8a7719df72d2414d8f067693c6f508268ce
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-12-04 21:50:44 +0000

    update

--- xdp_lb_kern.c
+++ xdp_lb_kern.c
@@ -1,12 +1,7 @@
 #include "xdp_lb_kern.h"
-#include <stdio.h>
 
 #define IP_ADDRESS(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
 
-#define BACKEND_A 2
-#define BACKEND_B 3
-#define CLIENT 4
-#define LB 5
 
 SEC("xdp")
 int xdp_load_balancer(struct xdp_md *ctx)
